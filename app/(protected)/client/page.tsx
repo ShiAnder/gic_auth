f962a906ed6dcef61f03c8a6cb554cc4d9511395
Peiris M. H. C. <154981814+IT22194930@users.noreply.github.com>
2025-03-18 10:49:22 +0530

    Implement Add Details Form with organization and service submission functionality

--- app/(protected)/client/page.tsx
+++ app/(protected)/client/page.tsx
@@ -1,18 +1,20 @@
 "use client";
 
-import { UserInfo } from "@/components/user-info";
+import { AddDetailsForm } from "@/components/add-details-form/AddDetailsForm";
 import { useCurrentUser } from "@/hooks/use-current-user";
 
 const ClientPage = () => {
-    const user = useCurrentUser();
+  const user = useCurrentUser();
 
-    if (!user) {
-        window.location.href = "/client";        
-    }
+  if (!user) {
+    window.location.href = "/client";
+  }
 
-    return (
-        <UserInfo label="📱 Client Component"  user={user}/>
-    );
-};  
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
+      <AddDetailsForm label="" user={user} />
+    </div>
+  );
+};
 
 export default ClientPage;
